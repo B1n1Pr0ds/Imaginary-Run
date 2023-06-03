a7aaa5d31c5833176256ee7add51b730e1b424d1
B1n1Pr0ds <123495359+B1n1Pr0ds@users.noreply.github.com>
2023-06-03 17:04:04 -0300

    Main menu and ame start

--- Assets/_Game/_Shared/GameManager.cs
+++ Assets/_Game/_Shared/GameManager.cs
@@ -1,11 +1,25 @@
 
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
-    void PauseGame()
+//Pause game by additive scene
+    public void PauseGame()
     {
+        SceneManager.LoadScene(2, LoadSceneMode.Additive);
+        Time.timeScale = 0;
+    }
 
+    public void ResumeGame()
+    {
+        SceneManager.UnloadScene(2);
+        Time.timeScale = 1;
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+        Time.timeScale = 1;
     }
 
 
